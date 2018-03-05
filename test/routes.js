a8b3b3da3b5bbb642a60e6c5a4f4615818b82818
David Guttman <david@davidguttman.com>
2018-03-05 09:03:35 -0800

    feat: add reverse route

--- test/routes.js
+++ test/routes.js
@@ -86,11 +86,19 @@ tape('should get stream', function (t) {
 
 tape('should get echo', function (t) {
   var url = '/echo?one=1&two=2'
+tape('should get reverse', function (t) {
+  var expected = {
+    input: 'stringtoreverse',
+    output: 'esreverotgnirts'
+  }
+
+  var url = '/reverse/' + expected.input
   servertest(server(), url, {encoding: 'json'}, function (err, res) {
     t.ifError(err, 'no error')
 
     t.equal(res.statusCode, 200, 'correct statusCode')
     t.deepEqual(res.body, {one: '1', two: '2'}, 'values should match')
+    t.deepEqual(res.body, expected, 'values should match')
     t.end()
   })
 })
